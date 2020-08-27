7725de20f3cfe85c848231e9d24b662c1c9a0d10
jhylands <jhylands@cassinisystems.com>
2020-08-27 22:13:21 +0100

    Updating to run with TF2

--- ctc_tensorflow_example.py
+++ ctc_tensorflow_example.py
@@ -99,30 +99,30 @@ def run_ctc():
         # e.g: log filter bank or MFCC features
         # Has size [batch_size, max_step_size, num_features], but the
         # batch_size and max_step_size can vary along each step
-        inputs = tf.placeholder(tf.float32, [None, None, num_features], name='inputs')
+        inputs = tf.compat.v1.placeholder(tf.float32, [None, None, num_features], name='inputs')
 
         # Here we use sparse_placeholder that will generate a
         # SparseTensor required by ctc_loss op.
         # https://www.tensorflow.org/api_docs/python/tf/sparse/SparseTensor
         # https://www.tensorflow.org/api_docs/python/tf/nn/ctc_loss
-        targets = tf.sparse_placeholder(tf.int32, name='targets')
+        targets = tf.compat.v1.sparse_placeholder(tf.int32, name='targets')
 
         # 1d array of size [batch_size]
-        seq_len = tf.placeholder(tf.int32, [None], name='seq_len')
+        seq_len = tf.compat.v1.placeholder(tf.int32, [None], name='seq_len')
 
         # Defining the cell
         # Can be:
         #   tf.nn.rnn_cell.RNNCell
         #   tf.nn.rnn_cell.GRUCell
-        cell = tf.contrib.rnn.LSTMCell(num_hidden, state_is_tuple=True)
+        cell = tf.compat.v1.nn.rnn_cell.LSTMCell(num_hidden, state_is_tuple=True)
 
         # Stacking rnn cells
-        stack = tf.contrib.rnn.MultiRNNCell([cell], state_is_tuple=True)
+        stack = tf.compat.v1.nn.rnn_cell.MultiRNNCell([cell], state_is_tuple=True)
 
         # The second output is the last state and we will no use that
-        outputs, _ = tf.nn.dynamic_rnn(stack, inputs, seq_len, dtype=tf.float32)
+        outputs, _ = tf.compat.v1.nn.dynamic_rnn(stack, inputs, seq_len, dtype=tf.float32)
 
-        shape = tf.shape(inputs)
+        shape = tf.shape(input=inputs)
         batch_s, max_time_steps = shape[0], shape[1]
 
         # Reshaping to apply the same weights over the timesteps
@@ -131,7 +131,7 @@ def run_ctc():
         # Truncated normal with mean 0 and stdev=0.1
         # Tip: Try another initialization
         # see https://www.tensorflow.org/versions/r0.9/api_docs/python/contrib.layers.html#initializers
-        W = tf.Variable(tf.truncated_normal([num_hidden,
+        W = tf.Variable(tf.random.truncated_normal([num_hidden,
                                              num_classes],
                                             stddev=0.1))
         # Zero initialization
@@ -145,26 +145,26 @@ def run_ctc():
         logits = tf.reshape(logits, [batch_s, -1, num_classes])
 
         # Time major
-        logits = tf.transpose(logits, (1, 0, 2))
+        logits = tf.transpose(a=logits, perm=(1, 0, 2))
 
-        loss = tf.nn.ctc_loss(targets, logits, seq_len)
-        cost = tf.reduce_mean(loss)
+        loss = tf.compat.v1.nn.ctc_loss(targets, logits, seq_len)
+        cost = tf.reduce_mean(input_tensor=loss)
 
         # optimizer = tf.train.AdamOptimizer().minimize(cost)
         # optimizer = tf.train.MomentumOptimizer(learning_rate=0.01, momentum=0.9).minimize(cost)
-        optimizer = tf.train.AdamOptimizer(learning_rate=5e-4).minimize(cost)
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=5e-4).minimize(cost)
 
         # Option 2: tf.contrib.ctc.ctc_beam_search_decoder
         # (it's slower but you'll get better results)
         decoded, log_prob = tf.nn.ctc_greedy_decoder(logits, seq_len)
 
         # Inaccuracy: label error rate
-        ler = tf.reduce_mean(tf.edit_distance(tf.cast(decoded[0], tf.int32),
+        ler = tf.reduce_mean(input_tensor=tf.edit_distance(tf.cast(decoded[0], tf.int32),
                                               targets))
 
-    with tf.Session(graph=graph) as session:
+    with tf.compat.v1.Session(graph=graph) as session:
 
-        tf.global_variables_initializer().run()
+        tf.compat.v1.global_variables_initializer().run()
 
         for curr_epoch in range(num_epochs):
             train_cost = train_ler = 0
